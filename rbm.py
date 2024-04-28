9435d1a10525d48cfd99188e991e40469c847b2a
Kei Minagawa <minagawa@fun.bio.keio.ac.jp>
2024-04-28 21:08:23 +0900

    fixed RBM's h_state.shape=(1, hidden_size)->(batch)size, hidden_size)

--- rbm.py
+++ rbm.py
@@ -4,23 +4,27 @@ import torch.nn.functional as F
 
 
 class RBM(nn.Module):
-    def __init__(self, visible_size, hidden_size, k_cd=1, device="cpu"):
+    def __init__(self, visible_size, hidden_size, batch_size, k_cd=1, device="cpu"):
         super().__init__()
         self.visible_size = visible_size
         self.hidden_size = hidden_size
-        self.weight = nn.Parameter(
-            torch.normal(0, 0.01, size=[hidden_size, visible_size])
-        )
-        self.bias_v = nn.Parameter(torch.zeros([1, visible_size]))
-        self.bias_h = nn.Parameter(torch.zeros([1, hidden_size]))
-        self.h_state = torch.zeros([1, hidden_size]).to(device)
+        self.param = {
+            "weight": torch.normal(
+                0, 0.01, size=[hidden_size, visible_size], device=device
+            ),
+            "bias_v": torch.zeros([1, visible_size], device=device),
+            "bias_h": torch.zeros([1, hidden_size], device=device),
+        }
+        self.h_state = torch.zeros([batch_size, hidden_size], device=device)
         self.k_cd = k_cd
 
     def _pv_given_h(self, h):
-        return torch.sigmoid(F.linear(h, self.weight.t(), self.bias_v))
+        return torch.sigmoid(
+            F.linear(h, self.param["weight"].t(), self.param["bias_v"])
+        )
 
     def _ph_given_v(self, v):
-        return torch.sigmoid(F.linear(v, self.weight, self.bias_h))
+        return torch.sigmoid(F.linear(v, self.param["weight"], self.param["bias_h"]))
 
     def _sample_pcd(self, v):
         # persistent constractive divergence(k)法によるサンプリング
@@ -44,34 +48,67 @@ class RBM(nn.Module):
 
         return h_pos, v_neg, h_neg
 
-    def grad(self, v, sample_type="pcd"):
+    def _sample_pcd_cont(self, v):
+        # persistent constractive divergence(k)法によるサンプリング
+        ph_pos = self._ph_given_v(v)
+
+        h_neg = self.h_state
+        for _ in range(self.k_cd):
+            v_neg = self._pv_given_h(h_neg).bernoulli()
+            ph_neg = self._ph_given_v(v_neg)
+            h_neg = ph_neg.bernoulli()
+        self.h_state = h_neg
+
+        return ph_pos, v_neg, ph_neg
+
+    def grad(self, v, sample_type="pcd_cont"):
         if sample_type == "pcd":
             h_pos, v_neg, h_neg = self._sample_pcd(v)
         elif sample_type == "cd":
             h_pos, v_neg, h_neg = self._sample_cd(v)
+        elif sample_type == "pcd_cont":
+            h_pos, v_neg, h_neg = self._sample_pcd_cont(v)
         else:
-            raise NotImplementedError("choose pcd pr cd")
+            raise NotImplementedError("choose pcd, cd, or pcd_cont")
         B, _ = v.shape
-        hv_pos = torch.einsum("BH,BV->HV", h_pos, v)
-        hv_neg = torch.einsum("BH,BV->HV", h_neg, v_neg)
-        w_grad = (hv_pos - hv_neg) / B
+        w_grad = torch.matmul(h_pos.t(), v) - torch.matmul(h_neg.t(), v_neg)
+        w_grad = w_grad / B
         bh_grad = (h_pos - h_neg).mean(dim=0)
         bv_grad = (v - v_neg).mean(dim=0)
         return {"weight": w_grad, "bias_h": bh_grad, "bias_v": bv_grad}
 
-    def update(self, grad):
-        param = self.state_dict()
+    # def update(self, grad):
+    #     param = self.state_dict()
+    #     for key, value in grad.items():
+    #         param[key] += value
+
+    def update(self, v, lr, sample_type="pcd_cont"):
+        grad = self.grad(v, sample_type)
         for key, value in grad.items():
-            param[key] += value
+            self.param[key] += lr * value
 
     def free_energy(self, v):
         # 訓練データによる平均自由エネルギーを計算する
-        term1 = -torch.matmul(v, self.bias_v.squeeze(0))
-        bias = self.bias_h + torch.matmul(v, self.weight.t())
-        term2 = -torch.log(1 + torch.exp(bias)).sum(dim=1)
+        term1 = -torch.matmul(v, self.param["bias_v"].squeeze(0))
+        bias = self.bias_h + torch.matmul(v, self.param["weight"].t())
+        term2 = -torch.log(1 + torch.exp(bias) + 1e-10).sum(dim=1)
         return term1 + term2
 
-    def sample_by_v(self, v, num_gib=1000):
+    def energy(self, v):
+        v_term = torch.matmul(v, self.param["bias_v"].t())
+        w_x_h = torch.matmul(v, self.param["weight"].t()) + self.param["bias_h"]
+        h_term = torch.sum(F.softplus(w_x_h), dim=1)
+        return -h_term - v_term
+
+    def pseudo_likelihood(self, v):
+        flip = torch.randint(0, v.size(1), (1,))
+        v_fliped = v.clone()
+        v_fliped[:, flip] = 1 - v_fliped[:, flip]
+        energy = self.energy(v)
+        energy_fliped = self.energy(v_fliped)
+        return v.size(1) * F.softplus(energy_fliped - energy)
+
+    def sample_by_v(self, v, num_gib=300):
         # batchを受け取り、データを再構成する
         h = self._ph_given_v(v).bernoulli()
         for _ in range(num_gib):
@@ -79,3 +116,121 @@ class RBM(nn.Module):
             v_gibb = pv_gibb.bernoulli()
             h = self._ph_given_v(v_gibb).bernoulli()
         return v_gibb, pv_gibb
+
+
+class RBMNew(nn.Module):
+    def __init__(self, vis_dim, hid_dim, initial_std=0.01, device="cpu"):
+        super(RBM, self).__init__()
+        self.device = device
+        self.b = torch.zeros(1, vis_dim, device=device)
+        self.c = torch.zeros(1, hid_dim, device=device)
+        self.w = torch.empty((hid_dim, vis_dim), device=device).normal_(
+            mean=0, std=initial_std
+        )
+
+    def _visible_to_hidden(self, v):
+        """可視ユニットから隠れユニットをサンプル"""
+        p = torch.sigmoid(F.linear(v, self.w, self.c))
+        return p.bernoulli()
+
+    def _hidden_to_visible(self, h):
+        """隠れユニットから可視ユニットをサンプル"""
+        p = torch.sigmoid(F.linear(h, self.w.t(), self.b))
+        return p.bernoulli()
+
+    def _visible_to_ph(self, v):
+        """P(h=1|v)を計算"""
+        return torch.sigmoid(F.linear(v, self.w, self.c))
+
+    def sample(self, v, gib_num=1):
+        """データをサンプリング"""
+        v = v.view(-1, self.w.size(1)).to(self.device)
+        h = self._visible_to_hidden(v)
+        for _ in range(gib_num):
+            v_gibb = self._hidden_to_visible(h)
+            h = self._visible_to_hidden(v_gibb)
+        return v_gibb
+
+    def sample_ph(self, v, gib_num=15):
+        """phをサンプリング"""
+        v = v.view(-1, self.w.size(1)).to(self.device)
+        ph = self._visible_to_ph(v)
+        h = ph.bernoulli()
+        # Gibbs Sampling 1 ~ k
+        for _ in range(gib_num):
+            v_gibb = self._hidden_to_visible(h)
+            ph_gibb = self._visible_to_ph(v_gibb)
+            h = ph_gibb.bernoulli()
+        return ph_gibb
+
+    def energy(self, v):
+        """エネルギーを計算"""
+        v_term = torch.matmul(v, self.b.t())
+        w_x_h = torch.matmul(v, self.w.t()) + self.c
+        h_term = torch.sum(F.softplus(w_x_h), dim=1)
+        return -h_term - v_term
+
+    def pseudo_likelihood(self, v):
+        """疑似対数尤度を計算"""
+        flip = torch.randint(0, v.size(1), (1,))
+        v_fliped = v.clone()
+        v_fliped[:, flip] = 1 - v_fliped[:, flip]
+        energy = self.energy(v)
+        energy_fliped = self.energy(v_fliped)
+        return v.size(1) * F.softplus(energy_fliped - energy)
+
+    def _update(self, v_pos, lr=0.1):
+        """ミニバッチあたりの学習更新"""
+        # positive part
+        ph_pos = self._visible_to_ph(v_pos)
+        # negative part
+        v_neg = self._hidden_to_visible(self.h_states)
+        ph_neg = self._visible_to_ph(v_neg)
+
+        lr = lr / v_pos.size(0)
+        # Update W
+        update = torch.matmul(ph_pos.t(), v_pos) - torch.matmul(ph_neg.t(), v_neg)
+        self.w += lr * update
+        self.b += lr * torch.sum(v_pos - v_neg, dim=0)
+        self.c += lr * torch.sum(ph_pos - ph_neg, dim=0)
+
+        # PCDのために隠れユニットの値を保持
+        self.h_states = ph_neg.bernoulli()
+
+    def fit(self, data, n_epoch=10, lr=1e-1, batch_size=128):
+        train = MyDataset(data[: int(len(data) * 0.7)])
+        test = MyDataset(data[int(len(data) * 0.7) :])
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train, batch_size=batch_size, shuffle=True, num_workers=0
+        )
+        test_loader = torch.utils.data.DataLoader(
+            dataset=train, batch_size=batch_size, shuffle=True, num_workers=0
+        )
+
+        train_loss_avg, val_loss_avg = [], []
+
+        # pcd memory
+        self.h_states = torch.zeros(batch_size, self.w.size(0), device=device)
+
+        for epoch in tqdm(range(n_epoch)):
+            train_loss_avg.append(0)
+            val_loss_avg.append(0)
+
+            self.train()
+            for i, data in enumerate(train_loader):
+                data = data.to(self.device)
+                self._update(data)
+                train_loss_avg[-1] += -self.pseudo_likelihood(data).mean().item()
+            train_loss_avg[-1] /= data.size(1)
+
+            self.eval()
+            with torch.no_grad():
+                for i, data in enumerate(test_loader):
+                    data = data.view(-1, self.w.size(1)).to(self.device)
+                    val_loss_avg[-1] += -self.pseudo_likelihood(data).mean().item()
+                val_loss_avg[-1] /= data.size(1)
+
+            print(
+                f"[EPOCH]: {epoch+1}, [LOSS]: {train_loss_avg[-1]:.4f}, [VAL]: {val_loss_avg[-1]:.4f}"
+            )
+        return train_loss_avg, val_loss_avg
--- train.py
+++ train.py
@@ -21,6 +21,14 @@ def imshow(grid, path):
     plt.savefig(path)
 
 
+def reconst_loss(prob, target):
+    target = torch.nn.functional.one_hot(target.long())  # (B, I, 2)
+    prob = torch.stack([prob, 1 - prob], dim=-1)  # (B, I, 2)
+    loss = target * torch.log(prob + 1e-10)
+    loss = loss.sum(dim=[-1, -2])
+    return loss.mean()
+
+
 if __name__ == "__main__":
     # argument parser
     parser = argparse.ArgumentParser("Restricted Boltzman Machine")
@@ -31,16 +39,21 @@ if __name__ == "__main__":
     )
     parser.add_argument("--hidden_size", type=int, default=256)
     parser.add_argument("--k_cd", type=int, default=1)
-    parser.add_argument("--lr", type=float, default=0.01)
-    parser.add_argument("--optim", type=str, default="adam")
-    parser.add_argument("--grad_steps", type=int, default=5000)
+    parser.add_argument(
+        "--sampling_method",
+        type=str,
+        default="pcd_cont",
+        choices=["pcd", "cd", "pcd_cont"],
+    )
+    parser.add_argument("--lr", type=float, default=0.1)
+    parser.add_argument("--optim", type=str, default="sgd")
+    parser.add_argument("--num_epoch", type=int, default=100)
     parser.add_argument("--batch_size", type=int, default=60)
     parser.add_argument("--log_dir", type=str, default="log")
     parser.add_argument("--fig_dir", type=str, default="fig")
-    parser.add_argument("--log_interval", type=int, default=1000)
     parser.add_argument("--device_id", type=str, default="cuda:0")
-    parser.add_argument("--num_gib", type=int, default=1000)
-    parser.add_argument("--result_format", type=str, default="pdf")
+    parser.add_argument("--num_gib", type=int, default=300)
+    parser.add_argument("--result_format", type=str, default="png")
     args = parser.parse_args()
 
     # tensorboard
@@ -54,7 +67,7 @@ if __name__ == "__main__":
         [
             transforms.ToTensor(),
             transforms.Lambda(lambda x: x.view(-1)),
-            transforms.Lambda(lambda x: torch.where(x < 0.5, 0.0, 1.0)),
+            transforms.Lambda(lambda x: torch.where(x <= 0.5, 0.0, 1.0)),
         ]
     )
     train_sets = MNIST(root="./data", train=True, transform=transform, download=True)
@@ -68,10 +81,10 @@ if __name__ == "__main__":
     model = RBM(
         visible_size=28 * 28,
         hidden_size=args.hidden_size,
+        batch_size=args.batch_size,
         k_cd=args.k_cd,
         device=device,
-    ).to(device)
-    model.requires_grad_(False)  # no-use pytorch BP
+    )
     if args.optim == "adam":
         optimizer = Adam(model.state_dict(), lr=args.lr)
     elif args.optim == "sgd":
@@ -81,19 +94,20 @@ if __name__ == "__main__":
 
     # train
     H, W = 28, 28
-    itr = 0
 
     train_loader_iter = iter(train_loader)
     test_loader_iter = iter(test_loader)
     inputs_train_const, _ = next(train_loader_iter)
     inputs_test_const, _ = next(test_loader_iter)
-    train_loader_iter = iter(train_loader)
-    test_loader_iter = iter(test_loader)
+    inputs_train_const, inputs_test_const = (
+        inputs_train_const.to(device),
+        inputs_test_const.to(device),
+    )
 
     sum_train_reconst = 0
     sum_test_reconst = 0
     metrics = {
-        "step": [],
+        "epoch": [],
         "fe_train": [],
         "fe_test": [],
         "fe_diff": [],
@@ -101,80 +115,80 @@ if __name__ == "__main__":
         "reconst_test": [],
     }
 
-    for step in tqdm(range(args.grad_steps)):
-        try:
-            inputs, _ = next(train_loader_iter)
-        except StopIteration:
-            train_loader_iter = iter(train_loader)
-            inputs, _ = next(train_loader_iter)
+    for epoch in tqdm(range(args.num_epoch)):
+        sum_train_reconst = 0
+        sum_test_reconst = 0
+        sum_train_pl = 0
+        sum_test_pl = 0
+        for inputs, _ in train_loader:
+            inputs = inputs.to(device)
+            # grad = model.grad(inputs, sample_type="pcd_cont")
+            # grad = optimizer.calc_grad(grad)
+            model.update(inputs, args.lr, args.sampling_method)
+            outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
+            sum_train_reconst += reconst_loss(prob, inputs).item()
+            sum_train_pl += -model.pseudo_likelihood(inputs).mean().item()
 
-        inputs = inputs.to(device)
-        grad = model.grad(inputs, sample_type="pcd")
-        grad = optimizer.calc_grad(grad)
-        model.update(grad)
+        with torch.no_grad():
+            for inputs, _ in test_loader:
+                inputs = inputs.to(device)
+                outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
+                sum_test_reconst += reconst_loss(prob, inputs).item()
+                sum_test_pl += -model.pseudo_likelihood(inputs).mean().item()
 
-        try:
-            inputs_test, _ = next(test_loader_iter)
-        except StopIteration:
-            test_loader_iter = iter(test_loader_iter)
-            inputs_test, _ = next(test_loader_iter)
+        outputs, prob = model.sample_by_v(inputs_train_const, num_gib=args.num_gib)
+        inputs = inputs_train_const.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
+        outputs = outputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
 
-        # reconst error
-        outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
-        train_reconst = (inputs * torch.log(prob)).sum(-1).mean()
-        sum_train_reconst += train_reconst
-        outputs_test, prob_test = model.sample_by_v(inputs_test, num_gib=args.num_gib)
-        test_reconst = (inputs * torch.log(prob_test)).sum(-1).mean()
-        sum_test_reconst += test_reconst
+        diff = ((inputs - outputs) + 1) / 2
+        images = torch.cat([inputs[:10], outputs[:10], diff[:10]], dim=0)
+        writer.add_image("reconst_img", make_grid(images, nrow=10), epoch)
 
-        itr += 1
+        # calculate mean free-energy
+        free_energy_train = model.free_energy(inputs_train_const).mean().item()
+        free_energy_test = model.free_energy(inputs_test_const).mean().item()
+        free_energy_diff = free_energy_train - free_energy_test
+        writer.add_scalar("free_energy_train", free_energy_train, epoch)
+        writer.add_scalar("free_energy_test", free_energy_test, epoch)
+        writer.add_scalar("free_energy_diff", free_energy_diff, epoch)
 
-        if step % args.log_interval:
-            # inputs, reconst, diff images
-            outputs, prob = model.sample_by_v(inputs_train_const, num_gib=args.num_gib)
-            inputs = inputs_train_const.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
-            outputs = outputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
-            diff = ((inputs - outputs) + 1) / 2
-            images = torch.cat([inputs[:10], outputs[:10], diff[:10]], dim=0)
-            writer.add_image("reconst_img", make_grid(images, nrow=10), step)
+        # calculate mean reconst loss
+        train_reconst = sum_train_reconst / len(train_loader)
+        test_reconst = sum_test_reconst / len(test_loader)
+        train_pl = sum_train_pl / len(train_loader)
+        test_pl = sum_test_pl / len(test_loader)
+        writer.add_scalar("train_reconst", train_reconst, epoch)
+        writer.add_scalar("test_reconst", test_reconst, epoch)
+        writer.add_scalar("train_pl", train_pl, epoch)
+        writer.add_scalar("test_pl", test_pl, epoch)
 
-            # calculate mean free-energy
-            free_energy_train = model.free_energy(inputs_train_const).mean()
-            free_energy_test = model.free_energy(inputs_test_const).mean()
-            free_energy_diff = free_energy_train - free_energy_test
-            writer.add_scalar("free_energy_train", free_energy_train, step)
-            writer.add_scalar("free_energy_test", free_energy_test, step)
-            writer.add_scalar("free_energy_diff", free_energy_diff, step)
+        # metrics
+        metrics["epoch"].append(epoch)
+        metrics["fe_train"].append(free_energy_train)
+        metrics["fe_test"].append(free_energy_test)
+        metrics["fe_diff"].append(free_energy_diff)
+        metrics["reconst_train"].append(train_reconst)
+        metrics["reconst_test"].append(test_reconst)
+        if epoch % 10 == 0:
+            print(
+                f"train_reconst_loss={train_reconst}, test_reconst_loss={test_reconst}"
+            )
+            print(f"train_pl={train_pl}, test_pl={test_pl}")
 
-            # calculate mean reconst loss
-            train_reconst = sum_train_reconst / itr
-            test_reconst = sum_test_reconst / itr
-            writer.add_scalar("train_reconst", train_reconst, step)
-            writer.add_scalar("test_reconst", test_reconst, step)
-            itr = 0
-
-            # metrics
-            metrics["step"].append(step)
-            metrics["fe_train"].append(free_energy_train)
-            metrics["fe_test"].append(free_energy_test)
-            metrics["fe_diff"].append(free_energy_diff)
-            metrics["reconst_train"].append(train_reconst)
-            metrics["reconst_test"].append(test_reconst)
-
-    # result pdf
     try:
         inputs, _ = next(train_loader_iter)
+        inputs = inputs.to(device)
     except StopIteration:
         train_loader_iter = iter(train_loader)
         inputs, _ = next(train_loader_iter)
-    outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
+        inputs = inputs.to(device)
 
+    outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
     inputs = inputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
     outputs = outputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
     diff = ((inputs - outputs) + 1) / 2
-
     images = torch.cat([inputs[:10], outputs[:10], diff[:10]], dim=0)
-    grid = make_grid(images, nrow=10)
+    grid = make_grid(images, nrow=10).cpu()
     format = args.result_format
     fig_dir = f"./{args.fig_dir}"
     os.makedirs(fig_dir, exist_ok=True)
@@ -182,22 +196,22 @@ if __name__ == "__main__":
     imshow(grid, path)
 
     plt.figure()
-    plt.plot(metrics["step"], metrics["reconst_train"], label="train", marker=".")
-    plt.plot(metrics["step"], metrics["reconst_test"], label="test", marker=".")
-    plt.xlabel("step")
+    plt.plot(metrics["epoch"], metrics["reconst_train"], label="train", marker=".")
+    plt.plot(metrics["epoch"], metrics["reconst_test"], label="test", marker=".")
+    plt.xlabel("epoch")
     plt.ylabel("reconst_loss")
     plt.savefig(f"{fig_dir}/result_reconst.{format}")
 
     plt.figure()
-    plt.plot(metrics["step"], metrics["fe_train"], label="train", marker=".")
-    plt.plot(metrics["step"], metrics["fe_test"], label="test", marker=".")
-    plt.xlabel("step")
+    plt.plot(metrics["epoch"], metrics["fe_train"], label="train", marker=".")
+    plt.plot(metrics["epoch"], metrics["fe_test"], label="test", marker=".")
+    plt.xlabel("epoch")
     plt.ylabel("free_energy")
     plt.savefig(f"{fig_dir}/result_fe.{format}")
 
     plt.figure()
-    plt.plot(metrics["step"], metrics["fe_diff"], marker=".")
-    plt.xlabel("step")
+    plt.plot(metrics["epoch"], metrics["fe_diff"], marker=".")
+    plt.xlabel("epoch")
     plt.ylabel("free_energy_diff")
     plt.savefig(f"{fig_dir}/result_fe_diff.{format}")
 
