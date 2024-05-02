7d1f5faaac547f5660b4b22302621a3c56364d2b
Kei Minagawa <minagawa@fun.bio.keio.ac.jp>
2024-05-02 14:22:07 +0900

    commit test....testtest

--- rbm.py
+++ rbm.py
@@ -1,6 +1,8 @@
+from matplotlib import _label_from_arg
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Categorical
 
 
 class RBM(nn.Module):
@@ -27,7 +29,9 @@ class RBM(nn.Module):
         return torch.sigmoid(F.linear(v, self.param["weight"], self.param["bias_h"]))
 
     def _sample_pcd(self, v):
-        # persistent constractive divergence(k)法によるサンプリング
+        """
+        persistent constractive divergence(k)法によるサンプリング
+        """
         h_pos = self._ph_given_v(v).bernoulli()
         h_neg = self.h_state
         for _ in range(self.k_cd):
@@ -38,18 +42,21 @@ class RBM(nn.Module):
         return h_pos, v_neg, h_neg
 
     def _sample_cd(self, v):
-        # constractive divergence(k)法によるサンプリング
+        """
+        constractive divergence(k)法によるサンプリング
+        """
         h_pos = self._ph_given_v(v).bernoulli()
         h_neg = h_pos
         for _ in range(self.k_cd):
             v_neg = self._pv_given_h(h_neg).bernoulli()
             h_neg = self._ph_given_v(v_neg).bernoulli()
-        self.h_state = h_neg
 
         return h_pos, v_neg, h_neg
 
     def _sample_pcd_cont(self, v):
-        # persistent constractive divergence(k)法によるサンプリング
+        """
+        persistent constractive divergence(k)法によるサンプリング
+        """
         ph_pos = self._ph_given_v(v)
 
         h_neg = self.h_state
@@ -62,6 +69,9 @@ class RBM(nn.Module):
         return ph_pos, v_neg, ph_neg
 
     def grad(self, v, sample_type="pcd_cont"):
+        """
+        生成モデルの勾配計算
+        """
         if sample_type == "pcd":
             h_pos, v_neg, h_neg = self._sample_pcd(v)
         elif sample_type == "cd":
@@ -77,42 +87,256 @@ class RBM(nn.Module):
         bv_grad = (v - v_neg).mean(dim=0)
         return {"weight": w_grad, "bias_h": bh_grad, "bias_v": bv_grad}
 
-    # def update(self, grad):
-    #     param = self.state_dict()
-    #     for key, value in grad.items():
-    #         param[key] += value
+    def grad_regressor(self, v, y):
+        """
+        v:(B, I), y:(B)
+        自由エネルギーによる回帰を行う場合の勾配計算
+        """
+        h = self._ph_given_v(v)
+        fe = self.free_energy(v)  # (B)
+        error = fe - y
+        w_grad = torch.einsum("B,BH,BI->HI", error, h, v)
+        bh_grad = torch.einsum("B,BH->H", error, h)
+        bv_grad = torch.einsum("B,BI->I", error, v)
+        return {"weight": w_grad, "bias_h": bh_grad, "bias_v": bv_grad}
 
-    def update(self, v, lr, sample_type="pcd_cont"):
-        grad = self.grad(v, sample_type)
+    def update(self, grad):
+        """
+        optimizerで計算した勾配でパラメータを更新
+        """
         for key, value in grad.items():
-            self.param[key] += lr * value
+            self.param[key] += value
+
+    # def update(self, v, lr, sample_type="pcd_cont"):
+    #     grad = self.grad(v, sample_type)
+    #     for key, value in grad.items():
+    #         self.param[key] += lr * value
 
     def free_energy(self, v):
-        # 訓練データによる平均自由エネルギーを計算する
+        """
+        データから自由エネルギーを計算
+        """
         term1 = -torch.matmul(v, self.param["bias_v"].squeeze(0))
-        bias = self.bias_h + torch.matmul(v, self.param["weight"].t())
+        bias = self.param["bias_h"] + torch.matmul(v, self.param["weight"].t())
         term2 = -torch.log(1 + torch.exp(bias) + 1e-10).sum(dim=1)
         return term1 + term2
 
-    def energy(self, v):
+    def _energy(self, v):
+        """
+        ボルツマンマシンのエネルギーを計算
+        """
         v_term = torch.matmul(v, self.param["bias_v"].t())
         w_x_h = torch.matmul(v, self.param["weight"].t()) + self.param["bias_h"]
         h_term = torch.sum(F.softplus(w_x_h), dim=1)
         return -h_term - v_term
 
     def pseudo_likelihood(self, v):
+        """
+        学習の進行を確認するための疑似対数尤度を計算
+        """
         flip = torch.randint(0, v.size(1), (1,))
         v_fliped = v.clone()
         v_fliped[:, flip] = 1 - v_fliped[:, flip]
-        energy = self.energy(v)
-        energy_fliped = self.energy(v_fliped)
+        energy = self._energy(v)
+        energy_fliped = self._energy(v_fliped)
         return v.size(1) * F.softplus(energy_fliped - energy)
 
     def sample_by_v(self, v, num_gib=300):
-        # batchを受け取り、データを再構成する
+        """
+        データから隠れ変数をサンプルして可視変数をサンプルする
+        """
         h = self._ph_given_v(v).bernoulli()
         for _ in range(num_gib):
             pv_gibb = self._pv_given_h(h)
             v_gibb = pv_gibb.bernoulli()
             h = self._ph_given_v(v_gibb).bernoulli()
         return v_gibb, pv_gibb
+
+
+class RBMClassification(nn.Module):
+    """
+    class labelとdataの同時分布をモデル化するボルツマンマシン
+    """
+
+    def __init__(
+        self, visible_size, label_size, hidden_size, batch_size, k_cd=1, device="cpu"
+    ):
+        super().__init__()
+        self.visible_size = visible_size
+        self.label_size = label_size
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+        self.device = device
+        self.param = {
+            "weight1": torch.normal(
+                0, 0.01, size=[hidden_size, visible_size], device=device
+            ),
+            "weight2": torch.normal(
+                0, 0.01, size=[hidden_size, label_size], device=device
+            ),
+            "bias_v1": torch.zeros([1, visible_size], device=device),
+            "bias_v2": torch.zeros([1, label_size], device=device),
+            "bias_h": torch.zeros([1, hidden_size], device=device),
+        }
+        self.h_state = torch.zeros([batch_size, hidden_size], device=device)
+        self.k_cd = k_cd
+
+    def _pv_data_given_h(self, h):
+        return torch.sigmoid(
+            F.linear(h, self.param["weight1"].t(), self.param["bias_v1"])
+        )
+
+    def _pv_class_given_h(self, h):
+        return F.softmax(
+            F.linear(h, self.param["weight2"].t(), self.param["bias_v2"]), dim=-1
+        )
+
+    def _ph_given_v(self, v, label):
+        v = torch.cat([v, label], dim=-1)
+        weight = torch.cat([self.param["weight1"], self.param["weight2"]], dim=1)
+        return torch.sigmoid(F.linear(v, weight, self.param["bias_h"]))
+
+    def _sample_pcd(self, v, label):
+        """
+        persistent constractive divergence(k)法によるサンプリング
+        """
+        h_pos = self._ph_given_v(v, label).bernoulli()
+        h_neg = self.h_state
+        for _ in range(self.k_cd):
+            v_data_neg = self._pv_data_given_h(h_neg).bernoulli()
+            v_class_neg = Categorical(self._pv_class_given_h(h_neg)).sample()
+            h_neg = self._ph_given_v(v_data_neg, v_class_neg).bernoulli()
+        self.h_state = h_neg
+
+        return h_pos, v_data_neg, v_class_neg, h_neg
+
+    def _sample_cd(self, v, label):
+        """
+        constractive divergence(k)法によるサンプリング
+        """
+        h_pos = self._ph_given_v(v, label).bernoulli()
+        h_neg = h_pos
+        for _ in range(self.k_cd):
+            v_data_neg = self._pv_data_given_h(h_neg).bernoulli()
+            v_class_neg = Categorical(self._pv_class_given_h(h_neg)).sample()
+            h_neg = self._ph_given_v(v_data_neg, v_class_neg).bernoulli()
+
+        return h_pos, v_data_neg, v_class_neg, h_neg
+
+    def _sample_pcd_cont(self, v, label):
+        """
+        persistent constractive divergence(k)法によるサンプリング
+        """
+        ph_pos = self._ph_given_v(v, label)
+
+        h_neg = self.h_state
+        for _ in range(self.k_cd):
+            v_data_neg = self._pv_data_given_h(h_neg).bernoulli()
+            v_class_neg = Categorical(self._pv_class_given_h(h_neg)).sample()
+            ph_neg = self._ph_given_v(v_data_neg, v_class_neg)
+            h_neg = ph_neg.bernoulli()
+        self.h_state = h_neg
+
+        return ph_pos, v_data_neg, v_class_neg, ph_neg
+
+    def grad(self, v, label, sample_type="pcd_cont"):
+        """
+        生成モデルの勾配計算
+        """
+        if sample_type == "pcd":
+            h_pos, v_data_neg, v_class_neg, h_neg = self._sample_pcd(v, label)
+        elif sample_type == "cd":
+            h_pos, v_data_neg, v_class_neg, h_neg = self._sample_cd(v, label)
+        elif sample_type == "pcd_cont":
+            h_pos, v_data_neg, v_class_neg, h_neg = self._sample_pcd_cont(v, label)
+        else:
+            raise NotImplementedError("choose pcd, cd, or pcd_cont")
+        B, _ = v.shape
+        w1_grad = torch.matmul(h_pos.t(), v) - torch.matmul(h_neg.t(), v_data_neg)
+        w1_grad = w1_grad / B
+        w2_grad = torch.matmul(h_pos.t(), label) - torch.matmul(h_neg.t(), v_class_neg)
+        w2_grad = w2_grad / B
+        bh_grad = (h_pos - h_neg).mean(dim=0)
+        bv1_grad = (v - v_data_neg).mean(dim=0)
+        bv2_grad = (label - v_class_neg).mean(dim=0)
+        return {
+            "weight1": w1_grad,
+            "weight2": w2_grad,
+            "bias_h": bh_grad,
+            "bias_v1": bv1_grad,
+            "bias_v2": bv2_grad,
+        }
+
+    def update(self, grad):
+        """
+        optimizerで計算した勾配でパラメータを更新
+        """
+        for key, value in grad.items():
+            self.param[key] += value
+
+    def free_energy(self, v, label):
+        """
+        データから自由エネルギーを計算
+        """
+        term1 = -torch.matmul(v, self.param["bias_v1"].squeeze(0))
+        term2 = -torch.matmul(label, self.param["bias_v2"].squeeze(0))
+        bias = (
+            self.param["bias_h"]
+            + torch.matmul(v, self.param["weight1"].t())
+            + torch.matmul(label, self.param["weight2"].t())
+        )
+        term2 = -torch.log(1 + torch.exp(bias) + 1e-10).sum(dim=1)
+        return term1 + term2
+
+    def _energy(self, v, label):
+        """
+        ボルツマンマシンのエネルギーを計算
+        """
+        v_term = torch.matmul(v, self.param["bias_v1"].t())
+        label_term = torch.matmul(label, self.param["bias_v2"].t())
+        w_v_h = torch.matmul(v, self.param["weight1"].t()) + self.param["bias_h"]
+        w_label_h = (
+            torch.matmul(label, self.param["weight2"].t()) + self.param["bias_h"]
+        )
+        h_v_term = torch.sum(F.softplus(w_v_h), dim=1)
+        h_label_term = torch.sum(F.softplus(w_label_h), dim=1)
+        return -h_v_term - h_label_term - v_term - label_term
+
+    def pseudo_likelihood(self, v, label):
+        """
+        学習の進行を確認するための疑似対数尤度を計算
+        """
+        flip = torch.randint(0, v.size(1) + label.size(1), (1,))
+        v_fliped = v.clone()
+        v_fliped[:, flip] = 1 - v_fliped[:, flip]
+        label_fliped = v.clone()
+        label_fliped[:, flip] = 1 - label_fliped[:, flip]
+        energy = self._energy(v, label)
+        energy_fliped = self._energy(v_fliped, label_fliped)
+        return (v.size(1) + label.size(1)) * F.softplus(energy_fliped - energy)
+
+    def sample_by_v(self, v, label, num_gib=300):
+        """
+        データが与えられた条件で可視変数をサンプルする
+        """
+        h = self._ph_given_v(v, label).bernoulli()
+        for _ in range(num_gib):
+            pv_data_gibb = self._pv_data_given_h(h)
+            pv_class_gibb = self._pv_class_given_h(h)
+            v_data_gibb = pv_data_gibb.bernoulli()
+            v_class_gibb = Categorical(pv_class_gibb).sample()
+            h = self._ph_given_v(v_data_gibb, v_class_gibb).bernoulli()
+        return v_data_gibb, v_class_gibb, pv_data_gibb, pv_class_gibb
+
+    def classification(self, v):
+        """
+        自由エネルギーによるクラス分類
+        """
+        fe_list = []
+        for i in range(self.label_size):
+            labels = torch.zeros([self.batch_size, self.label_size], device=self.device)
+            labels[:, i] = 1  # one-hotベクトルを作成
+            fe_list.append(self.free_energy(v, labels))
+        fe_tensor = torch.stack(fe_list, dim=-1)  # (B, label_size)
+        pred_label = torch.argmax(fe_tensor, dim=-1)  # (B)
+        return pred_label
--- train_classification.py
+++ train_classification.py
@@ -1,17 +1,40 @@
-import torch
-from torchvision.utils import make_grid
-from torchvision.datasets import MNIST
-from torchvision.transforms import transforms
-from torch.utils.data import DataLoader
-from rbm import RBM
+from operator import attrgetter
+from rbm import RBMClassification
 from optim import Adam, SGD
-from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.utils import make_grid
+from torchvision.transforms import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from tensorboardX import SummaryWriter
-
-import argparse
 import datetime
 import os
+import argparse
+
+
+def reconst_loss(probs, targets):
+    targets = F.one_hot(targets.long())  # (B, I, 2)
+    probs = torch.stack([probs, 1 - probs], dim=-1)  # (B, I, 2)
+    loss = targets * torch.log(probs + 1e-10)
+    loss = loss.sum(dim=[-1, -2])
+    return loss.mean()
+
+
+def categorical_cross_entropy(probs, labels):
+    loss = (labels * torch.log(probs + 1e-10)).sum(-1)
+    print("loss.shape = ", loss.shape)
+    return loss.mean()
+
+
+def accuracy(preds, labels):
+    labels = torch.argmax(labels, dim=-1)
+    acc = (preds == labels).sum()
+    acc /= labels.shape[0]
+    return acc
 
 
 def imshow(grid, path):
@@ -20,17 +43,10 @@ def imshow(grid, path):
     plt.savefig(path)
 
 
-def reconst_loss(prob, target):
-    target = torch.nn.functional.one_hot(target.long())  # (B, I, 2)
-    prob = torch.stack([prob, 1 - prob], dim=-1)  # (B, I, 2)
-    loss = target * torch.log(prob + 1e-10)
-    loss = loss.sum(dim=[-1, -2])
-    return loss.mean()
-
-
 if __name__ == "__main__":
-    # argument parser
-    parser = argparse.ArgumentParser("Restricted Boltzman Machine")
+    parser = argparse.ArgumentParser(
+        "Restricted Boltzmann Machine MNIST Classification"
+    )
     parser.add_argument(
         "--id",
         type=str,
@@ -48,8 +64,8 @@ if __name__ == "__main__":
     parser.add_argument("--optim", type=str, default="sgd")
     parser.add_argument("--num_epoch", type=int, default=100)
     parser.add_argument("--batch_size", type=int, default=60)
-    parser.add_argument("--log_dir", type=str, default="log")
-    parser.add_argument("--fig_dir", type=str, default="fig")
+    parser.add_argument("--log_dir", type=str, default="log_cl")
+    parser.add_argument("--fig_dir", type=str, default="fig_cl")
     parser.add_argument("--device_id", type=str, default="cuda:0")
     parser.add_argument("--num_gib", type=int, default=300)
     parser.add_argument("--result_format", type=str, default="png")
@@ -75,33 +91,37 @@ if __name__ == "__main__":
     train_loader = DataLoader(train_sets, batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(test_sets, batch_size=args.batch_size, shuffle=False)
 
-    # model, optimizer
+    # model
     device = args.device_id if torch.cuda.is_available() else "cpu"
-    model = RBM(
-        visible_size=28 * 28,
+    train_loader_iter = iter(train_loader)
+    test_loader_iter = iter(test_loader)
+    inputs_train_const, labels_train_const = next(train_loader_iter)
+    inputs_test_const, labels_test_const = next(test_loader_iter)
+    inputs_train_const, inputs_test_const = (
+        inputs_train_const.to(device),
+        inputs_test_const.to(device),
+    )
+    labels_train_const, labels_test_const = (
+        F.one_hot(labels_train_const).to(device),
+        F.one_hot(labels_test_const).to(device),
+    )
+
+    H, W, label_size = 28, 28, 10
+    model = RBMClassification(
+        visible_size=H * W,
+        label_size=label_size,
         hidden_size=args.hidden_size,
         batch_size=args.batch_size,
         k_cd=args.k_cd,
         device=device,
     )
+
     if args.optim == "adam":
         optimizer = Adam(model.param, lr=args.lr)
     elif args.optim == "sgd":
         optimizer = SGD(model.param, lr=args.lr)
     else:
         NotImplementedError()
-
-    # train
-    H, W = 28, 28
-
-    train_loader_iter = iter(train_loader)
-    test_loader_iter = iter(test_loader)
-    inputs_train_const, _ = next(train_loader_iter)
-    inputs_test_const, _ = next(test_loader_iter)
-    inputs_train_const, inputs_test_const = (
-        inputs_train_const.to(device),
-        inputs_test_const.to(device),
-    )
 
     sum_train_reconst = 0
     sum_test_reconst = 0
@@ -110,6 +130,8 @@ if __name__ == "__main__":
         "fe_train": [],
         "fe_test": [],
         "fe_diff": [],
+        "acc_train": [],
+        "acc_test": [],
         "reconst_train": [],
         "reconst_test": [],
         "pl_train": [],
@@ -117,51 +139,78 @@ if __name__ == "__main__":
     }
 
     for epoch in tqdm(range(args.num_epoch)):
+        sum_train_acc = 0
+        sum_test_acc = 0
         sum_train_reconst = 0
         sum_test_reconst = 0
+        sum_train_ce = 0
+        sum_test_ce = 0
         sum_train_pl = 0
         sum_test_pl = 0
-        for inputs, _ in train_loader:
-            inputs = inputs.to(device)
-            grad = model.grad(inputs, sample_type="pcd_cont")
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
+            grad = model.grad(inputs, labels, sample_type="pcd_cont")
             grad = optimizer.calc_grad(grad)
             model.update(grad)
-            # model.update(inputs, args.lr, args.sampling_method)
-            outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
-            sum_train_reconst += reconst_loss(prob, inputs).item()
-            sum_train_pl += -model.pseudo_likelihood(inputs).mean().item()
+            outputs_data, outputs_label, probs_data, probs_label = model.sample_by_v(
+                inputs, labels, num_gib=args.num_gib
+            )
+            preds = model.classification(inputs)
+            sum_train_acc += accuracy(preds, labels).item()
+            sum_train_reconst += reconst_loss(probs_data, inputs).item()
+            sum_train_ce += categorical_cross_entropy(probs_label, labels).item()
+            sum_train_pl += -model.pseudo_likelihood(inputs, labels).mean().item()
 
         with torch.no_grad():
-            for inputs, _ in test_loader:
-                inputs = inputs.to(device)
-                outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
-                sum_test_reconst += reconst_loss(prob, inputs).item()
-                sum_test_pl += -model.pseudo_likelihood(inputs).mean().item()
+            for inputs, labels in test_loader:
+                inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
+                outputs_data, outputs_label, probs_data, probs_label = (
+                    model.sample_by_v(inputs, labels, num_gib=args.num_gib)
+                )
+                preds = model.classification(inputs)
+                sum_test_acc += accuracy(preds, labels).item()
+                sum_test_reconst += reconst_loss(probs_data, inputs).item()
+                sum_test_ce += categorical_cross_entropy(probs_label, labels).item()
+                sum_test_pl += -model.pseudo_likelihood(inputs, labels).mean().item()
 
         # reconstruction
-        outputs, prob = model.sample_by_v(inputs_train_const, num_gib=args.num_gib)
-        inputs = inputs_train_const.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
-        outputs = outputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
+        outputs_data, outputs_label, probs_data, probs_label = model.sample_by_v(
+            inputs_train_const, labels_train_const, num_gib=args.num_gib
+        )
+        inputs_data = inputs_train_const.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
+        outputs_data = outputs_data.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
 
-        diff = ((inputs - outputs) + 1) / 2
-        images = torch.cat([inputs[:10], outputs[:10], diff[:10]], dim=0)
+        diff_data = ((inputs_data - outputs_data) + 1) / 2
+        images = torch.cat([inputs_data[:10], outputs_data[:10], diff_data[:10]], dim=0)
         writer.add_image("reconst_img", make_grid(images, nrow=10), epoch)
 
         # calculate mean free-energy
-        free_energy_train = model.free_energy(inputs_train_const).mean().item()
-        free_energy_test = model.free_energy(inputs_test_const).mean().item()
+        free_energy_train = (
+            model.free_energy(inputs_train_const, labels_train_const).mean().item()
+        )
+        free_energy_test = (
+            model.free_energy(inputs_test_const, labels_test_const).mean().item()
+        )
         free_energy_diff = free_energy_train - free_energy_test
         writer.add_scalar("free_energy_train", free_energy_train, epoch)
         writer.add_scalar("free_energy_test", free_energy_test, epoch)
         writer.add_scalar("free_energy_diff", free_energy_diff, epoch)
 
         # calculate mean reconstruction-loss
+        train_acc = sum_train_acc / len(train_loader)
+        test_acc = sum_test_acc / len(test_loader)
         train_reconst = sum_train_reconst / len(train_loader)
         test_reconst = sum_test_reconst / len(test_loader)
+        train_ce = sum_train_ce / len(train_loader)
+        test_ce = sum_test_ce / len(test_loader)
         train_pl = sum_train_pl / len(train_loader)
         test_pl = sum_test_pl / len(test_loader)
+        writer.add_scalar("acc_train", train_acc, epoch)
+        writer.add_scalar("acc_test", test_acc, epoch)
         writer.add_scalar("train_reconst", train_reconst, epoch)
         writer.add_scalar("test_reconst", test_reconst, epoch)
+        writer.add_scalar("train_ce", train_ce, epoch)
+        writer.add_scalar("test_ce", test_ce, epoch)
         writer.add_scalar("train_pl", train_pl, epoch)
         writer.add_scalar("test_pl", test_pl, epoch)
 
@@ -170,8 +219,12 @@ if __name__ == "__main__":
         metrics["fe_train"].append(free_energy_train)
         metrics["fe_test"].append(free_energy_test)
         metrics["fe_diff"].append(free_energy_diff)
+        metrics["acc_train"].append(train_acc)
+        metrics["acc_test"].append(test_acc)
         metrics["reconst_train"].append(train_reconst)
         metrics["reconst_test"].append(test_reconst)
+        metrics["ce_train"].append(train_ce)
+        metrics["ce_test"].append(test_ce)
         metrics["pl_train"].append(train_pl)
         metrics["pl_test"].append(test_pl)
 
@@ -180,19 +233,22 @@ if __name__ == "__main__":
                 f"train_reconst_loss={train_reconst}, test_reconst_loss={test_reconst}"
             )
             print(f"train_pl={train_pl}, test_pl={test_pl}")
+            print(f"train_ce={train_ce}, test_ce={test_ce}")
 
     # evaluate RBM
     try:
-        inputs, _ = next(train_loader_iter)
-        inputs = inputs.to(device)
+        inputs, labels = next(train_loader_iter)
+        inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
     except StopIteration:
         train_loader_iter = iter(train_loader)
-        inputs, _ = next(train_loader_iter)
-        inputs = inputs.to(device)
+        inputs, labels = next(train_loader_iter)
+        inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
 
-    outputs, prob = model.sample_by_v(inputs, num_gib=args.num_gib)
+    outputs_data, outputs_label, probs_data, probs_label = model.sample_by_v(
+        inputs, labels, num_gib=args.num_gib
+    )
     inputs = inputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
-    outputs = outputs.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
+    outputs = outputs_label.reshape(-1, 1, H, W).repeat(1, 3, 1, 1)
     diff = ((inputs - outputs) + 1) / 2
     images = torch.cat([inputs[:10], outputs[:10], diff[:10]], dim=0)
     grid = make_grid(images, nrow=10).cpu()
@@ -209,6 +265,14 @@ if __name__ == "__main__":
     plt.ylabel("reconst_loss")
     plt.legend()
     plt.savefig(f"{fig_dir}/result_reconst.{format}")
+
+    plt.figure()
+    plt.plot(metrics["epoch"], metrics["acc_train"], label="train", marker=".")
+    plt.plot(metrics["epoch"], metrics["acc_test"], label="test", marker=".")
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.legend()
+    plt.savefig(f"{fig_dir}/result_accuracy.{format}")
 
     plt.figure()
     plt.plot(metrics["epoch"], metrics["fe_train"], label="train", marker=".")
@@ -231,5 +295,22 @@ if __name__ == "__main__":
     plt.ylabel("pseudo_likelihood")
     plt.legend()
     plt.savefig(f"{fig_dir}/result_pl.{format}")
+
+    # evaluate accuracy
+    sum_train_acc = 0
+    sum_test_acc = 0
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
+        preds = model.classification(inputs)
+        sum_train_acc += accuracy(inputs, labels).item()
+
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), F.one_hot(labels).to(device)
+        preds = model.classification(inputs)
+        sum_test_acc += accuracy(inputs, labels).item()
+    train_acc = sum_train_acc / len(train_loader)
+    test_acc = sum_test_acc / len(test_loader)
+    print("=================================")
+    print(f"train_acc={train_acc}, test_acc={test_acc}")
 
     writer.close()
